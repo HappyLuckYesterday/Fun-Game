a024d5f113320bfcc31ed31a00346d6a2d107805
HappyYama <milesdanieldragon@gmail.com>
2017-08-11 18:51:54 +0200

    Add core test project

---
+++ test/Rhisis.Core.Test/CryptographyTest.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace Rhisis.Core.Test
+{
+    public class CryptographyTest
+    {
+        [Fact]
+        public void DecryptPasswordSuccess()
+        {
+        }
+
+        [Fact]
+        public void DecryptPasswordFailure()
+        {
+        }
+    }
+}
