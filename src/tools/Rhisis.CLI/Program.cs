c7b4950428f3a528a3e367b3df884824f7647ae8
HappyYama <milesdanieldragon@gmail.com>
2017-09-11 19:45:13 +0200

    Add Rhisis CLI project

---
+++ src/tools/Rhisis.CLI/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.Reflection;
+
+namespace Rhisis.CLI
+{
+    public class Program
+    {
+        private static readonly string Version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+
+        public static void Main(string[] args)
+        {
+            var app = new CommandLineApplication()
+            {
+                Name = "Rhisis.CLI.exe",
+                FullName = "Rhisis CLI",
+                Description = "Rhisis CLI (Command Line Interface)"
+            };
+
+            app.HelpOption("-?|-h|--help");
+            app.VersionOption("--version", Version);
+
+            try
+            {
+                app.Execute(args);
+            }
+            catch (CommandParsingException ex)
+            {
+                Console.WriteLine(ex.Message);
+                app.ShowHelp();
+            }
+        }
+    }
+}
