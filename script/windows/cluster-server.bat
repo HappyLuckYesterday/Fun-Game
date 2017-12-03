bee68670729406b61f8c46a1304c6d5f60bed9bc
HappyYama <milesdanieldragon@gmail.com>
2017-12-03 01:06:31 +0100

    Add windows build scripts for release

---
+++ script/windows/cluster-server.bat
@@ -0,0 +1,6 @@
+:: Author: Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
+:: Title: Rhisis Cluster Server launcher
+:: Description: This script starts the Rhisis Cluster Server.
+@echo OFF
+
+dotnet ./bin/cluster/Rhisis.Cluster.dll
