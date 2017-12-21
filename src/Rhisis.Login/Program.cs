195cc66e0a51dd7edfcb94d25fde89e1654f11fe
HappyYama <milesdanieldragon@gmail.com>
2017-12-21 11:24:27 +0100

    Update Rhisis.Login server for unhandled exception test

--- src/Rhisis.Login/Program.cs
+++ src/Rhisis.Login/Program.cs
@@ -1,17 +1,39 @@
 ﻿using System;
+using System.Runtime.Loader;
 
 namespace Rhisis.Login
 {
     public static class Program
     {
         private static readonly string ProgramTitle = "Rhisis - LoginServer";
+        private static LoginServer Server = null;
 
         private static void Main(string[] args)
         {
             Console.Title = ProgramTitle;
+            AssemblyLoadContext.Default.Unloading += Default_Unloading;
 
-            using (var server = new LoginServer())
-                server.Start();
+            try
+            {
+                Server = new LoginServer();
+                Server.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+#if DEBUG
+                Console.WriteLine(e.StackTrace);
+#endif
+            }
+            finally
+            {
+                Server.Dispose();
+            }
+        }
+
+        private static void Default_Unloading(AssemblyLoadContext obj)
+        {
+            Server?.Dispose();
         }
     }
 }
