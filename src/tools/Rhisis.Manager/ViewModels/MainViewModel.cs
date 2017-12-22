74b8e3cdd3c5981c39c7f32617383fd7ddc3ea02
HappyYama <milesdanieldragon@gmail.com>
2017-12-22 22:22:06 +0100

    Add Rhisis Manager tool

---
+++ src/tools/Rhisis.Manager/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Rhisis.Manager.Core.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rhisis.Manager.ViewModels
+{
+    public sealed class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<MenuItemViewModel> MenuItems { get; }
+
+        public MainViewModel()
+        {
+            this.MenuItems = new ObservableCollection<MenuItemViewModel>
+            {
+                new MenuItemViewModel("File", "/Resources/Images/Icons/document.png", null),
+                new MenuItemViewModel("Tools", "/Resources/Images/Icons/settings.png", null),
+                new MenuItemViewModel("About", "/Resources/Images/Icons/about.png", null)
+            };
+        }
+    }
+}
