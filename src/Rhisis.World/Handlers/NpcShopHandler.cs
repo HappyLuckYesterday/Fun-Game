d7f1b38d61c7162e3965c70a0bd414258e432eb9
HappyYama <milesdanieldragon@gmail.com>
2018-03-13 21:13:24 +0100

    Create OpenShopWindow packet and handler

---
+++ src/Rhisis.World/Handlers/NpcShopHandler.cs
@@ -0,0 +1,19 @@
+﻿using Ether.Network.Packets;
+using Rhisis.Core.Network;
+using Rhisis.Core.Network.Packets;
+using Rhisis.Core.Network.Packets.World;
+
+namespace Rhisis.World.Handlers
+{
+    public static class NpcShopHandler
+    {
+        [PacketHandler(PacketType.OPENSHOPWND)]
+        public static void OnOpenShopWindow(WorldClient client, NetPacketBase packet)
+        {
+            var openShopPacket = new OpenShopWindowPacket(packet);
+
+            // TODO: Get the correct NPC from the global context
+            // TODO: Send NPC shop to client
+        }
+    }
+}
