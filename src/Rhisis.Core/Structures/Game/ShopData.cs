32626b3f7e56f01b9f3818fec62e5e47830e2b9d
HappyYama <milesdanieldragon@gmail.com>
2018-03-13 19:46:02 +0100

    Add npc shop structure

---
+++ src/Rhisis.Core/Structures/Game/ShopData.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace Rhisis.Core.Structures.Game
+{
+    /// <summary>
+    /// Represents a NPC Shop data.
+    /// </summary>
+    [DataContract]
+    public class ShopData
+    {
+        /// <summary>
+        /// Gets or sets the shop name.
+        /// </summary>
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the shop items.
+        /// </summary>
+        [DataMember(Name = "items")]
+        public ShopItemData[] Items { get; set; }
+    }
+}
---
+++ src/Rhisis.Core/Structures/Game/ShopItemData.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization;
+
+namespace Rhisis.Core.Structures.Game
+{
+    /// <summary>
+    /// Represents a shop item.
+    /// </summary>
+    [DataContract]
+    public class ShopItemData
+    {
+        /// <summary>
+        /// Gets or sets the item id.
+        /// </summary>
+        [DataMember(Name = "itemId")]
+        public int ItemId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the item's refine.
+        /// </summary>
+        [DataMember(Name = "refine")]
+        public int Refine { get; set; }
+
+        /// <summary>
+        /// Gets or sets the item's element.
+        /// </summary>
+        [DataMember(Name = "element")]
+        public int Element { get; set; }
+
+        /// <summary>
+        /// Gets or sets the item's element refine.
+        /// </summary>
+        [DataMember(Name = "elementRefine")]
+        public int ElementRefine { get; set; }
+    }
+}
