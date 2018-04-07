1df6f738d72467dd7dfcf7bfd522f3602fd72ed1
HappyYama <milesdanieldragon@gmail.com>
2018-04-07 17:28:05 +0200

    Add /getgold Chat Command

---
+++ src/Rhisis.World/Game/Chat/GetGoldCommand.cs
@@ -0,0 +1,30 @@
+﻿using Rhisis.Core.Data;
+using Rhisis.Core.IO;
+using Rhisis.World.Game.Entities;
+using Rhisis.World.Packets;
+
+namespace Rhisis.World.Game.Chat
+{
+    public static class GetGoldChatCommand
+    {
+        [ChatCommand("/getgold")]
+        public static void GetGoldCommand(IPlayerEntity player, string[] parameters)
+        {
+            Logger.Debug("{0} want to get penyas", player.PlayerData.Gold);
+
+            if (parameters.Length == 1)
+            {
+                if (int.TryParse(parameters[0], out int GoldByCommand))
+                {
+                    player.PlayerData.Gold += GoldByCommand;
+                    WorldPacketFactory.SendUpdateAttributes(player, DefineAttributes.GOLD, player.PlayerData.Gold);
+                }
+            }
+            else
+            {
+                Logger.Error("Chat: /getgold command must have only one parameter.");
+                return;
+            }
+        }
+    }
+}
