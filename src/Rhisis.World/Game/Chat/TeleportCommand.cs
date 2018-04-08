d5fe23977a6bc4b039eaf4b17e4e95ce7fed7551
HappyYama <milesdanieldragon@gmail.com>
2018-04-08 22:30:32 +0200

    Add Teleport Command [Still randomly crashing]

---
+++ src/Rhisis.World/Game/Chat/TeleportCommand.cs
@@ -0,0 +1,57 @@
+﻿using Rhisis.Core.IO;
+using Rhisis.World.Game.Entities;
+using Rhisis.World.Packets;
+
+namespace Rhisis.World.Game.Chat
+{
+    public static class TeleportationCommand
+    {
+        [ChatCommand("/tp")]
+        [ChatCommand("/teleport")]
+        public static void TeleportCommand(IPlayerEntity player, string[] parameters)
+        {
+            Logger.Debug("{0} want to teleport", player.Object.Name);
+
+            if (parameters.Length >= 2 || parameters.Length <= 4)
+            {
+                float.TryParse(parameters[0], out float firstValue);
+                float.TryParse(parameters[1], out float secondValue);
+                player.Object.Position.X = firstValue;
+
+                if (parameters.Length == 2)
+                {
+                    player.Object.Position.Z = secondValue;
+                    WorldPacketFactory.SendPlayerSpawn(player);
+                    return;
+                }
+
+                float.TryParse(parameters[2], out float thirdValue);
+
+                if (parameters.Length == 3)
+                {
+                    player.Object.Position.Y = secondValue;
+                    player.Object.Position.Z = thirdValue;
+                    WorldPacketFactory.SendPlayerSpawn(player);
+                    return;
+                }
+
+                int.TryParse(parameters[0], out int mapIdValue);
+                float.TryParse(parameters[3], out float forthValue);
+                   
+                if (parameters.Length == 4)
+                {
+                    player.Object.MapId = mapIdValue;
+                    player.Object.Position.X = secondValue;
+                    player.Object.Position.Y = thirdValue;
+                    player.Object.Position.Z = forthValue;
+                    WorldPacketFactory.SendPlayerSpawn(player);
+                    return;
+                }
+            }
+            if (parameters.Length < 2 || parameters.Length > 4)
+            {
+                Logger.Error("Chat: /teleport command must have 2, 3 or 4 parameters.");
+            }
+        }
+    }
+}
