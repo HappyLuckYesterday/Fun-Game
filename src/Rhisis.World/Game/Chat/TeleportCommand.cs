1f6d1b38465c169459d88691c5dab5a42e57ce6d
HappyYama <milesdanieldragon@gmail.com>
2018-04-10 19:58:12 +0200

    [ADD] Teleport Chat Command
    Adding the teleport command chat via
    "/teleport" "/tp"
    
    2 parameters : posX PosZ
    3 parameters : MapId posX posZ
    4 parameters : MapId posX posY posZ

--- src/Rhisis.World/Game/Chat/TeleportCommand.cs
+++ src/Rhisis.World/Game/Chat/TeleportCommand.cs
@@ -16,35 +16,35 @@ namespace Rhisis.World.Game.Chat
             {
                 float.TryParse(parameters[0], out float firstValue);
                 float.TryParse(parameters[1], out float secondValue);
-                player.Object.Position.X = firstValue;
+
 
                 if (parameters.Length == 2)
                 {
+                    player.Object.Position.X = firstValue;
                     player.Object.Position.Z = secondValue;
                     WorldPacketFactory.SendPlayerTeleport(player);
                     return;
                 }
 
                 int.TryParse(parameters[0], out int mapIdValue);
-
-                /*IReadOnlyDictionary<int, Map> worldId = new Dictionary<int, Map>();
-
-                if (!worldId.TryGetValue(mapIdValue, out Map MapId))
-                {
-                    Logger.Warning("Cannot find map Id in define files: {0}. Please check you defineWorld.h file.",
-                        worldId);
-                    return;
-                }*/
-
                 float.TryParse(parameters[2], out float thirdValue);
 
                 if (parameters.Length == 3)
                 {
-                    player.Object.MapId = mapIdValue;
-                    player.Object.Position.X = secondValue;
-                    player.Object.Position.Z = thirdValue;
-                    WorldPacketFactory.SendPlayerTeleport(player);
-                    return;
+                    if (!WorldServer.Maps.TryGetValue(mapIdValue, out Map mapIdResult))
+                    {
+                        Logger.Error("Cannot find map Id in define files: {0}. Please check you defineWorld.h file.",
+                            mapIdValue);
+                        return;
+                    }
+                    else
+                    {
+                        player.Object.MapId = mapIdValue;
+                        player.Object.Position.X = secondValue;
+                        player.Object.Position.Z = thirdValue;
+                        WorldPacketFactory.SendPlayerTeleport(player);
+                        return;
+                    }
                 }
 
                 float.TryParse(parameters[3], out float forthValue);
