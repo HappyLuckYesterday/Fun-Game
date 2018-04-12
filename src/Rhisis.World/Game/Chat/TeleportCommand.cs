d0b72d96dba060f551396a494297344fe6d3e133
HappyYama <milesdanieldragon@gmail.com>
2018-04-12 17:42:47 +0200

    [ADD] Teleport Command with more lisibility

--- src/Rhisis.World/Game/Chat/TeleportCommand.cs
+++ src/Rhisis.World/Game/Chat/TeleportCommand.cs
@@ -1,6 +1,7 @@
 ﻿using Rhisis.Core.IO;
 using Rhisis.World.Game.Entities;
 using Rhisis.World.Packets;
+using System;
 
 namespace Rhisis.World.Game.Chat
 {
@@ -11,63 +12,74 @@ namespace Rhisis.World.Game.Chat
         public static void TeleportCommand(IPlayerEntity player, string[] parameters)
         {
             Logger.Debug("{0} want to teleport", player.Object.Name);
-
             switch (parameters.Length)
             {
                 case 2: // when you write 2 parameters in the command
                     TeleportationParameters.TeleportCommandTwoParam(player, parameters);
-                    return;
+                    break;
                 case 3: // when you write 3 parameters in the command
                     TeleportationParameters.TeleportCommandThreeParam(player, parameters);
-                    return;
+                    break;
                 case 4: // when you write 4 parameters in the command
                     TeleportationParameters.TeleportCommandFourParam(player, parameters);
-                    return;
+                    break;
                 default: // when you write more than 4 or less than 2 parameters in the command
                     Logger.Error("Chat: /teleport command must have 2, 3 or 4 parameters.");
                     return;
             }
+            WorldPacketFactory.SendPlayerTeleport(player);
         }
     }
     public static class TeleportationParameters
     {
         public static void TeleportCommandTwoParam(IPlayerEntity player, string[] parameters)
         {
-            float.TryParse(parameters[0], out float posXValue);
-            float.TryParse(parameters[1], out float posZValue);
+            if (!float.TryParse(parameters[0], out float posXValue) || !float.TryParse(parameters[1], out float posZValue))
+            {
+                throw new ArgumentException("You must write numbers for teleport command's parameters");
+            }
+
             player.Object.Position.X = posXValue;
             player.Object.Position.Z = posZValue;
-            WorldPacketFactory.SendPlayerTeleport(player);
         }
         public static void TeleportCommandThreeParam(IPlayerEntity player, string[] parameters)
         {
-            int.TryParse(parameters[0], out int mapIdValue);
-            float.TryParse(parameters[1], out float posXValue);
-            float.TryParse(parameters[2], out float posZValue);
+            if (!ushort.TryParse(parameters[0], out ushort mapIdValue) || !float.TryParse(parameters[1], out float posXValue)
+                || !float.TryParse(parameters[2], out float posZValue))
+            {
+                throw new ArgumentException("You must write numbers for teleport command's parameters");
+            }
+
             if (!WorldServer.Maps.TryGetValue(mapIdValue, out Map mapIdResult))
             {
                 Logger.Error("Cannot find map Id in define files: {0}. Please check you defineWorld.h file.",
                     mapIdValue);
+                return;
             }
-            else
-            {
-                player.Object.MapId = mapIdValue;
-                player.Object.Position.X = posXValue;
-                player.Object.Position.Z = posZValue;
-                WorldPacketFactory.SendPlayerTeleport(player);
-            }
+
+            player.Object.MapId = mapIdValue;
+            player.Object.Position.X = posXValue;
+            player.Object.Position.Z = posZValue;
         }
         public static void TeleportCommandFourParam(IPlayerEntity player, string[] parameters)
         {
-            int.TryParse(parameters[0], out int mapIdValue);
-            float.TryParse(parameters[1], out float posXValue);
-            float.TryParse(parameters[2], out float posYValue);
-            float.TryParse(parameters[3], out float posZValue);
+            if (!ushort.TryParse(parameters[0], out ushort mapIdValue) || !float.TryParse(parameters[1], out float posXValue)
+            || !float.TryParse(parameters[2], out float posYValue) || !float.TryParse(parameters[3], out float posZValue))
+            {
+                throw new ArgumentException("You must write numbers for teleport command's parameters");
+            }
+
+            if (!WorldServer.Maps.TryGetValue(mapIdValue, out Map mapIdResult))
+            {
+                Logger.Error("Cannot find map Id in define files: {0}. Please check you defineWorld.h file.",
+                    mapIdValue);
+                return;
+            }
+
             player.Object.MapId = mapIdValue;
             player.Object.Position.X = posXValue;
             player.Object.Position.Y = posYValue;
             player.Object.Position.Z = posZValue;
-            WorldPacketFactory.SendPlayerTeleport(player);
         }
     }
 }
