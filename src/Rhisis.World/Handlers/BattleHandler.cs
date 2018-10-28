9e2a60a969e63912eab604eccd47686750aa653e
HappyYama <milesdanieldragon@gmail.com>
2018-10-28 12:48:05 +0100

    Add melee attack handler

---
+++ src/Rhisis.World/Handlers/BattleHandler.cs
@@ -0,0 +1,21 @@
+﻿using Ether.Network.Packets;
+using NLog;
+using Rhisis.Network;
+using Rhisis.Network.Packets;
+using Rhisis.Network.Packets.World;
+
+namespace Rhisis.World.Handlers
+{
+    public static class BattleHandler
+    {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        [PacketHandler(PacketType.MELEE_ATTACK)]
+        public static void OnMeleeAttack(WorldClient client, INetPacketStream packet)
+        {
+            var meleePacket = new MeleeAttackPacket(packet);
+
+            Logger.Debug($"message: {meleePacket.AttackMessage}; Target: {meleePacket.ObjectId}; AttackSpeed: {meleePacket.WeaponAttackSpeed}");
+        }
+    }
+}
