9b6dd12802cd516175a32e0664ce4d0e9c1a2a1c
HappyYama <milesdanieldragon@gmail.com>
2018-10-28 12:58:55 +0100

    Add battle system empty class

---
+++ src/Rhisis.World/Systems/Battle/BattleSystem.cs
@@ -0,0 +1,19 @@
+﻿using Rhisis.World.Game.Core;
+using Rhisis.World.Game.Core.Systems;
+using System;
+
+namespace Rhisis.World.Systems.Battle
+{
+    [System(SystemType.Notifiable)]
+    public class BattleSystem : ISystem
+    {
+        /// <inheritdoc />
+        public WorldEntityType Type => WorldEntityType.Player | WorldEntityType.Monster;
+
+        /// <inheritdoc />
+        public void Execute(IEntity entity, SystemEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
