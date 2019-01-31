a36e910d662f43ba1ae4892d0d532d7af8e8418f
HappyYama <milesdanieldragon@gmail.com>
2019-01-31 08:51:59 +0100

    Add Rhisis.API project and /user/register route

---
+++ src/Rhisis.API/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Rhisis.Business.Models;
+
+namespace Rhisis.API.Controllers
+{
+    /// <summary>
+    /// Provides API routes to manage users.
+    /// </summary>
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        /// <summary>
+        /// Creates a new <see cref="UserController"/> instance.
+        /// </summary>
+        /// <param name="logger">Logger</param>
+        public UserController(ILogger<UserController> logger)
+        {
+            this._logger = logger;
+        }
+        
+        /// <summary>
+        /// Registers an user.
+        /// </summary>
+        /// <param name="registerModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult RegisterUser([FromBody] UserRegisterModel registerModel)
+        {
+            return Ok();
+        }
+    }
+}
