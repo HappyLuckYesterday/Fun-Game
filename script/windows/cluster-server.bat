3528140134a2c87bbc6bc2c039087eb838da3157
HappyYama <milesdanieldragon@gmail.com>
2019-09-01 19:42:40 +0200

    Remove script folder

--- script/windows/cluster-server.bat
+++
@@ -1,6 +0,0 @@
-:: Author: Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
-:: Title: Rhisis Cluster Server launcher
-:: Description: This script starts the Rhisis Cluster Server.
-@echo OFF
-
-dotnet ./bin/cluster/Rhisis.Cluster.dll
--- script/windows/login-server.bat
+++
@@ -1,6 +0,0 @@
-:: Author: Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
-:: Title: Rhisis Login Server launcher
-:: Description: This script starts the Rhisis Login Server.
-@echo OFF
-
-dotnet ./bin/login/Rhisis.Login.dll
