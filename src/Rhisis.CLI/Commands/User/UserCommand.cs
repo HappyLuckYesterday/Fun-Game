e6163d684bc0588151ffcbdefc362f4c5a88c322
HappyYama <milesdanieldragon@gmail.com>
2020-01-30 23:26:28 -0300

    #319 missed the subcommand decorator

--- src/Rhisis.CLI/Commands/User/UserCommand.cs
+++ src/Rhisis.CLI/Commands/User/UserCommand.cs
@@ -5,6 +5,7 @@ namespace Rhisis.CLI.Commands.User
     [Command("user", Description = "Manages the database users")]
     [Subcommand(typeof(UserCreateCommand))]
     [Subcommand(typeof(UserShowCommand))]
+    [Subcommand(typeof(UserUpdateCommand))]
     public sealed class UserCommand
     {
         public void OnExecute(CommandLineApplication app) => app.ShowHelp();
