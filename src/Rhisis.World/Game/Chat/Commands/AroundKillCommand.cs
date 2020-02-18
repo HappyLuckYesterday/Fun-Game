fe9d35bf989bb71b6a14ad8e8d82698588bbdd0f
HappyYama <milesdanieldragon@gmail.com>
2020-02-18 14:21:52 +0100

    Ability to hit all monsters around a player

---
+++ src/Rhisis.World/Game/Chat/Commands/AroundKillCommand.cs
@@ -0,0 +1,51 @@
+using Microsoft.Extensions.Logging;
+using Rhisis.Core.Common;
+using Rhisis.World.Game.Entities;
+using Rhisis.World.Systems.PlayerData;
+using Rhisis.World.Systems.Battle;
+using Rhisis.Core.Data;
+using Rhisis.World.Systems.Follow;
+
+namespace Rhisis.World.Game.Chat
+{
+    [ChatCommand("/aroundkill", AuthorityType.Administrator)]
+    public class AroundKillChatCommand : IChatCommand
+    {
+        private readonly ILogger<AroundKillChatCommand> _logger;
+        private readonly IPlayerDataSystem _playerDataSystem;
+        private readonly IBattleSystem _battleSystem;
+        private readonly IFollowSystem _followSystem;
+
+        /// <summary>
+        /// Creates a new <see cref="AroundKillChatCommand"/> instance.
+        /// </summary>
+        /// <param name="logger">Logger.</param>
+        /// <param name="playerDataSystem">Player data system.</param>
+        /// <param name="battlePacketFactory">Battle packet factory.</param>
+        public AroundKillChatCommand(ILogger<AroundKillChatCommand> logger, IPlayerDataSystem playerDataSystem, IBattleSystem battleSystem,IFollowSystem followSystem)
+        {
+            _logger = logger;
+            _playerDataSystem = playerDataSystem;
+            _battleSystem = battleSystem;
+            _followSystem = followSystem;
+        }
+
+        /// <inheritdoc />
+        public void Execute(IPlayerEntity player, object[] parameters)
+        {
+            ObjectMessageType defaultAttackType = ObjectMessageType.OBJMSG_ATK1;
+            float defaultAttackSpeed = 1.0f;
+
+            for (int i = 0; i < player.Object.Entities.Count; i++)
+            {
+                if (player.Object.Entities[i] is IMonsterEntity monsterEntity) 
+                {
+                    if (!monsterEntity.Follow.IsFollowing) {
+                        _followSystem.Follow(monsterEntity, player);
+                    }
+                    _battleSystem.MeleeAttack(player, monsterEntity, defaultAttackType, defaultAttackSpeed);
+                }
+            }
+        }
+    }
+}
