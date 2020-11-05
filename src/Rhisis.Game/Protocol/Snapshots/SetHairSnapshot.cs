7dd66c510bc306a1a459c90e2ffeea6c684d8780
HappyYama <milesdanieldragon@gmail.com>
2020-11-05 19:49:03 +0100

    Add customization handlers

---
+++ src/Rhisis.Game/Protocol/Snapshots/SetHairSnapshot.cs
@@ -0,0 +1,16 @@
+﻿using Rhisis.Game.Abstractions.Entities;
+
+namespace Rhisis.Network.Snapshots
+{
+    public class SetHairSnapshot : FFSnapshot
+    {
+        public SetHairSnapshot(IPlayer player, byte hairId, byte r, byte g, byte b)
+            : base(SnapshotType.SET_HAIR, player.Id)
+        {
+            Write(hairId);
+            Write(r);
+            Write(g);
+            Write(b);
+        }
+    }
+}
