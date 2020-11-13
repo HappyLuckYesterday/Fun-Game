454ce3e049ce02c405eab1d48a458e29daf0907f
HappyYama <milesdanieldragon@gmail.com>
2020-11-13 22:10:46 +0100

    Correctly fix world server crash on unimplemented packet

--- bin/1.login.bat
+++ bin/1.login.bat
@@ -1,1 +1,2 @@
+title Rhisis - Login Server
 dotnet run --project ../src/Rhisis.Login/Rhisis.Login.csproj --configuration Debug
--- bin/2.cluster.bat
+++ bin/2.cluster.bat
@@ -1,1 +1,2 @@
+title Rhisis - Cluster Server
 dotnet run --project ../src/Rhisis.Cluster/Rhisis.Cluster.csproj --configuration Debug
