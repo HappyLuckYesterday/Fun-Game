8386f6d7a8ed189ab9fd44d2461d26781a00a7de
HappyYama <milesdanieldragon@gmail.com>
2023-04-25 14:36:26 +0200

    fix(emoticons): remove command & update ChatHandler

--- src/Rhisis.WorldServer/Handlers/ChatHandler.cs
+++ src/Rhisis.WorldServer/Handlers/ChatHandler.cs
@@ -4,6 +4,7 @@ using Rhisis.Game.Protocol.Packets.World.Client;
 using Rhisis.Protocol;
 using Rhisis.Protocol.Handlers;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -12,27 +13,58 @@ namespace Rhisis.WorldServer.Handlers;
 [PacketHandler(PacketType.CHAT)]
 internal sealed class ChatHandler : WorldPacketHandler
 {
+    private static readonly List<string> _emotes = new()
+    {
+      "/laugh",
+      "/sad",
+      "/kiss",
+      "/surprise",
+      "/blush",
+      "/anger",
+      "/sigh",
+      "/wink",
+      "/ache",
+      "/hunger",
+      "/yummy",
+      "/sneer",
+      "/sparkle",
+      "/ridicule",
+      "/sleepy",
+      "/rich",
+      "/glare",
+      "/sweat",
+      "/cat face",
+      "/tongue",
+      "/mad",
+      "/aha",
+      "/embarrassed",
+      "/help",
+      "/crazy",
+      "/oh!",
+      "/confused",
+      "/ouch",
+      "/love",
+    };
+
     public void Execute(ChatPacket packet)
     {
         if (!string.IsNullOrEmpty(packet.Message))
         {
             if (packet.Message.StartsWith("/"))
             {
-                string commandName = packet.Message.Split(' ', StringSplitOptions.RemoveEmptyEntries).FirstOrDefault();
-                IChatCommand chatCommand = ChatCommandManager.Current.Get(commandName, Player.Authority) ??
+                if (_emotes.Contains(packet.Message))
+                {
+                    Player.Speak(packet.Message);
+                }
+                else
+                {
+                    string commandName = packet.Message.Split(' ', StringSplitOptions.RemoveEmptyEntries).FirstOrDefault();
+                    IChatCommand chatCommand = ChatCommandManager.Current.Get(commandName, Player.Authority) ??
                      throw new ArgumentException($"Cannot find chat command: '/{commandName}'", nameof(chatCommand));
 
-                string[] commandParameters = GetCommandParameters(packet.Message, commandName, chatCommand.ParsingType);
-
-                // EMOTICON Part : BEGIN
-                if (chatCommand.GetType().Name == "GetEmoticonsCommand")
-                {   
-                    Player.Speak(packet.Message);
-                } else
-                {
-                    chatCommand.Execute(Player, commandParameters);
+                    string[] commandParameters = GetCommandParameters(packet.Message, commandName, chatCommand.ParsingType);
                 }
-                // EMOTICON Part : END
+
             }
             else
             {
