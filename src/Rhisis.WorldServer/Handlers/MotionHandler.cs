c1c909522e24e7de20b164941dae9870e4261473
HappyYama <milesdanieldragon@gmail.com>
2023-05-05 23:20:38 +0200

    MotionHandler remove constructor

--- src/Rhisis.WorldServer/Handlers/MotionHandler.cs
+++ src/Rhisis.WorldServer/Handlers/MotionHandler.cs
@@ -9,11 +9,6 @@ namespace Rhisis.WorldServer.Handlers;
 [PacketHandler(PacketType.MOTION)]
 internal sealed class MotionHandler : WorldPacketHandler
 {
-    /// <summary>
-    /// Creates a new <see cref="MotionHandler"/> instance.
-    /// </summary>
-    public MotionHandler() { }
-
     public void Execute(MotionPacket packet)
     {
         Player.Motion(packet.MotionEnum);
